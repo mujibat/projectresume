e2c2d068ec4f49b80d4412ff1cf0786ae80d94a5
mujibat <oshikoyadolapo13@gmail.com>
2021-08-23 10:28:55 +0100

    update

--- config/settings.py
+++ config/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ciba',
+        'NAME': 'sendemail',
     }
 }
 
